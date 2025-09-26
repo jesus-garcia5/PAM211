b030f9d6ae7d06b8dfe1ab44f3d73203f50e823f
jesus-garcia5 <124049284@upq.edu.mx>
2025-09-25 22:41:28 -0600

    Create main.js

---
+++ practica3/main.js
@@ -0,0 +1,8 @@
+
+import { restar } from './utils.js';
+
+console.log(" restar 5, 3 =", restar(5, 3));
+console.log(" restar 10, 5 =", restar(10, 5) ); 
+console.log(" restar 7, 8 =", restar(7, 8 ) );  
+console.log(" restar 0, 0 =", restar(0, 0) );  
+console.log(" restar -4, -14 =", restar(-4, -14) );
